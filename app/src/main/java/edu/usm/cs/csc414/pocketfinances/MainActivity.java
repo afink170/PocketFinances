9f6cc0667057359e997a27faf9c9212de107365e
afink170 <afink170@yahoo.com>
2018-09-17 10:47:06 -0500

    Testing commit and push functionality.

--- app/src/main/java/edu/usm/cs/csc414/pocketfinances/MainActivity.java
+++ app/src/main/java/edu/usm/cs/csc414/pocketfinances/MainActivity.java
@@ -2,12 +2,23 @@ package edu.usm.cs.csc414.pocketfinances;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewNum = findViewById(R.id.textViewNum);
+
+        textViewNum.setText(getText(R.string.hello_world_str));
+    }
+
+    public int add(int a, int b) {
+        return a+b;
     }
 }
